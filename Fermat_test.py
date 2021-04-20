de1d1601c2e0862ee7bacb04192615ae2e629287
Plasma Stark <64229723+PlasmaStark@users.noreply.github.com>
2021-04-20 11:31:37 +0200

    Readability

--- Fermat_test.py
+++ Fermat_test.py
@@ -1,13 +1,15 @@
 # very basic script for a primality test via Fermat's little theorem
 
 print("   [Fermat Test: check if n is (probably) prime!]   ")
-print("- insert n number to test & b base (Fermat witness) -")
+print("# insert n number to test & b base (Fermat witness) #")
 print("-----------------------------------------------------")
 
-cycle = 1 # count iterations
+cycle = 1 # counts iterations
 
 # main loop, only stops if user gets bored
 while True:
+    
+    # get user input --------------------------------------
     if cycle == 1: 
         n = int(input("-> insert wannabe-prime n: ")) 
         b = int(input("-> insert base b: ")) # witness
@@ -16,16 +18,22 @@ while True:
         b = int(input("-> insert new base b: "))
     print("Testing " + str(n) + " with base " +str(b), end='')
     
+    # test user input & tries to continue -----------------
     rem = pow(b,n-1,n)
-    if rem == 1:
+    
+    if rem == 1: # test returns true
         print("... " + str(n) + " is probably prime! ", end='')
-        if cycle != 1: # is user bored enough?
+        if cycle != 1: 
             print("Still probable prime at cycle " + str(cycle) + ".")
-        go_on = input("Want to try further? (Y=yes, N=no) ")
+        
+        # is user bored enough?
+        go_on = input("Want to try further? (Y=yes, N=no) ") 
         if(go_on == "N"): 
             break # then stop...
+            
     else: # test failed, no need to reiterate
         print("..." + str(n) + " is NOT prime! Remainder " + str(rem) + " is not 1. [working in mod" + str(n) + "]")
         if cycle != 1: 
             print("failed at cycle " + str(cycle))
+            
     cycle += 1
